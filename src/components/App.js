9ef17aaae7bb7010d5f42bc9782a8cd7973e1693
B6H7 <xyboy9024@gmail.com>
2022-07-28 10:06:28 +0900

    finish

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      // addEventListener와 유사한 형태. AuthState의 변화감지
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+      } else {
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  const refreshUser = () => {
+    // Navigation의 profile은 firebase가 아닌 react요소이기 때문에 바로바로 업데이트가 안된다.
+    const user = authService.currentUser;
+    setUserObj({
+      // userObj의 크기가 방대하기때문에 필요한 부분만 가져와서 직접 firebase의 정보를 업데이트 해준다.
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
+  return (
+    <>
+      {init ? (
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
+      ) : (
+        "Initializing"
+      )}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/Profile.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { authService, dbService } from "fbase";
+import { useHistory } from "react-router-dom";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ refreshUser,userObj }) => {
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+    refreshUser()
+  };
+  // const getMyNweets = async () => {
+  //   const nweets = await dbService
+  //     .collection("nweets")
+  //     .where("creatorId", "==", userObj.uid) // where => 필터링 작업
+  //     .orderBy("createdAt") // orderBy(정렬기준,asc or desc) => 정렬
+  //     .get(); // 이러한 쿼리를 firebase에 요청하려면 index가 필요하다. 다행히 firebase에서 index생성링크를 콘솔 error를 통해 제공한다
+  // };
+  // useEffect(() => {
+  //   getMyNweets();
+  // });
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
